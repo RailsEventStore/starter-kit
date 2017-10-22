a2470a33347ddc7c3420d9fec12b2ce67f638492
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2017-10-22 13:44:03 +0200

    make command_bus available in the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
 
   private
 
-
   def event_store
     @event_store ||= Rails.configuration.event_store.tap do |client|
       # client.subscribe(->(event) {do_sth_cool_with(event)}, [SomeEvent])
     end
   end
+
+  def command_bus
+    @command_bus ||= Arkency::CommandBus.new.tap do |command_bus|
+      # command_bus.register(
+      #   FooBoundedContext::DoSthCool,
+      #   FooBoundedContext::DoSthCoolHandler.new(event_store)
+      # )
+    end
+  end
+
 end
