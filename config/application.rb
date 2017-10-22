e8754b99da26f3f32f8f9f4edcec035a8e7d010f
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2017-10-22 13:38:51 +0200

    prepare AggregateRoot to work with event_store

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'aggregate_root'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,4 +11,8 @@ module StarterKit
   class Application < Rails::Application
     config.event_store = RailsEventStore::Client.new
   end
+end
+
+AggregateRoot.configure do |config|
+  config.default_event_store = Rails.configuration.event_store
 end
